271a42124dcf3c4aee46073855d12df8a059ca8f
rishi0698 <192682443+rishi0698@users.noreply.github.com>
2026-03-30 11:23:43 +0530

    phase 2: html form structure complete

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    today = datetime.now().strftime("%d-%b-%Y,%A")
+    return render_template("index.html", date=today)
 
 if __name__ == "__main__":
     app.run(debug=True)
